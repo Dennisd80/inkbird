d22645857c9d64a78c2507edab5cf097719c9543
Dennisd80 <d_donker@hotmail.com>
2020-07-05 13:51:02 +0200

    Update mqtt.py

--- inkbird/mqtt.py
+++ inkbird/mqtt.py
@@ -8,20 +8,18 @@ class MqttController:
     
     def setup(self):
         host = os.environ.get("INKBIRD_MQTT_HOST")
-        port = int(os.environ.get("INKBIRD_MQTT_PORT", 1883)
+        port = int(os.environ.get("INKBIRD_MQTT_PORT", 1883))
         user = os.environ.get("INKBIRD_MQTT_USER")
         password = os.environ.get("INKBIRD_MQTT_PASSWORD")
- 
+        
         client = mqtt.Client(client_id="inkbird")
         client.will_set("inkbird/status", payload="offline", retain=True, qos=0)
 
         def on_connect(client, userdata, flags, rc):
             client.publish("inkbird/status", "online", retain=True, qos=0)
 
-        client.username_pw_set("user", "password")
-        
         client.on_connect = on_connect
-
+        client.username_pw_set(user, password)
         client.connect(host=host, port=port)
         client.loop_start()
 
